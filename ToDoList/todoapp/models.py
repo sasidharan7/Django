e7085ec42ac028ebc76113574040a1c22992097d
sasidharan7 <47827750+sasidharan7@users.noreply.github.com>
2022-10-21 14:54:35 +0530

    Delete ToDoList directory

--- ToDoList/todoapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Task(models.Model):
-    tasks = models.CharField(max_length=200)
-    duedate = models.DateField(blank=True)
-
-class DoneTask(models.Model):
-    donetasks = models.CharField(max_length=200)
